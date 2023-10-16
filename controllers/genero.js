4fafd3c30af655d7d02299900a4c33d736e2a2b1
CDaza7 <cristian.daza@gmail.com>
2023-10-16 13:53:42 -0500

    CRUD completo generos

--- controllers/genero.js
+++ controllers/genero.js
@@ -50,19 +50,68 @@ const getGeneros = async (req = request, res = response) => {
 
 }
 
-// CONSULTAR UN GENERO POR SU ID
+// CONSULTAR UN GENERO POR SU NOMBRE
 
+const getGeneroNombre = async (req = request, res = response) => {
+    try {
+        const { nombre } = req.params;
 
+        const generos = await Genero.findOne(nombre);
+
+        if (!genero) {
+            return res.status(404).json({ msj: 'Género no encontrado' });
+        }
+
+        return res.json(generos);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ msj: error });
+    }
+}
 
 // ACTUALIZAR UN GENERO
+const updateGenero = async (req = request, res = response) => {
+    try {
+        const { nombre } = req.params;
+        const { descripcion } = req.body;
 
+        const generos = await Genero.findOneAndUpdate({ nombre }, { descripcion }, { new: true });
+
+        if (!generos) {
+            return res.status(404).json({ msj: 'Género no encontrado' });
+        }
+
+        return res.json(generos);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ msj: error });
+    }
+}
 
 
 // ELIMINAR UN GENERO
+const deleteGenero = async (req = request, res = response) => {
+    try {
+        const { nombre } = req.params;
+        const generos = await Genero.findOneAndDelete({ nombre });
+
+        if (!generos) {
+            return res.status(404).json({ msj: 'Género no encontrado' });
+        }
+
+        return res.json({ msj: 'Género eliminado con éxito' });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ msj: error });
+    }
+}
 
 
 
 module.exports = {
     createGenero,
-    getGeneros
+    getGeneros,
+    getGeneroNombre,
+    updateGenero,
+    deleteGenero
 }
--- routes/genero.js
+++ routes/genero.js
@@ -1,14 +1,21 @@
 const { Router } = require('express')
-const { createGenero, getGeneros } = require('../controllers/genero')
+const { createGenero, getGeneros, getGeneroNombre, updateGenero, deleteGenero} = require('../controllers/genero')
 const router = Router()
 
 // Crear un genero
 router.post('/', createGenero)
 
+// Consultar todos los generos
+router.get('/', getGeneros)
+
+// Consultar genero por nombre
+router.get('/', getGeneroNombre)
+
+//Actualizar un genero
+router.put('/:nombre', updateGenero);
+
+//Eliminar un genero
+router.delete('/:nombre', deleteGenero);
 
 
 module.exports = router
-
-// Consultar todos los generos
-
-router.get('/', getGeneros)
